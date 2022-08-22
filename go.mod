501ffbb82d6eb1fb19281f7fa8fc36987399f948
hayama17 <hanetarou01@gmail.com>
2022-08-22 12:52:31 +0000

    add ns_user & test netns

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/hayama17/go_container
 
 go 1.19
 
-require github.com/containerd/cgroups v1.0.4
+require (
+	github.com/containerd/cgroups v1.0.4
+	github.com/vishvananda/netns v0.0.0-20211101163701-50045581ed74
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007
+)
 
 require (
 	github.com/cilium/ebpf v0.4.0 // indirect
@@ -11,5 +15,4 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 
 	cgroupsv2 "github.com/containerd/cgroups/v2"
+	"github.com/vishvananda/netns"
 )
 
 func main() {
@@ -28,13 +29,33 @@ func parent() error {
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        10,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        10,
+			},
+		},
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
+	if err := cmd.Start(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	}
+	NsHandle, _ := netns.GetFromPid(cmd.Process.Pid)
+	fmt.Println(NsHandle.String())
+	if err := cmd.Wait(); err != nil {
 		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
@@ -43,60 +64,67 @@ func parent() error {
 }
 
 func child() error {
+	//set veth
+	// NsHandle, _ := netns.NewNamed("kohei")
+	// netns.Set(NsHandle)
+	//set hostname
+	log.Println("set hostname")
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		return fmt.Errorf("Setting hostname failed: %w", err)
+	}
+
+	//cgroup meory limit
 	minMem := int64(1)                 // 1K
 	maxMem := int64(500 * 1024 * 1024) //100M
 	res := cgroupsv2.Resources{
 		Memory: &cgroupsv2.Memory{
-			// values are in bytes: https://www.kernel.org/doc/html/latest/admin-guide/cgroup-v2.html#memory-interface-files
 			Min: &minMem,
 			Max: &maxMem,
 		},
 	}
+	log.Println("create cgroup maneger")
 	mgr, err := cgroupsv2.NewManager("/sys/fs/cgroup", "/go-container-cgroupv2", &res)
-
 	if err != nil {
 		return fmt.Errorf("creating cgroups v2: %w", err)
 	}
 	defer mgr.Delete()
 
+	log.Println("register tasks to my-container")
 	if err := ioutil.WriteFile("/sys/fs/cgroup/go-container-cgroupv2/cgroup.procs", []byte(fmt.Sprintf("%d\n", os.Getpid())), 0644); err != nil {
 		return fmt.Errorf("Cgroups register tasks to my-container namespace failed: %w", err)
 	}
-	log.Println("prepareRoot")
+
+	//pivot root
+	log.Println("prepare Rootfs")
 	if err := syscall.Mount("", "/", "", syscall.MS_SLAVE|syscall.MS_REC, ""); err != nil {
-		return fmt.Errorf("prepare RootFS: %w", err)
+		return fmt.Errorf("prepare Rootfs: %w", err)
 	}
 
 	log.Println("mkdir newroot/putold")
 	if err := os.MkdirAll("newroot/putold", 0755); err != nil {
 		return fmt.Errorf("creating directory: %w", err)
 	}
-	log.Println("bind mount to ./newroot")
+	log.Println("bind mount ./newroot")
 	if err := syscall.Mount("newroot", "newroot", "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
 		return fmt.Errorf("bind mounting newroot: %w", err)
 	}
 
-	log.Println("run pivot_root")
+	log.Println("pivot_root")
 	if err := syscall.PivotRoot("newroot", "newroot/putold"); err != nil {
 		return fmt.Errorf("pivot root: %w", err)
 	}
 
-	log.Println("===========================================")
-	log.Println("chdir / and go inside pivot root jail. Now we are in created container!")
-	log.Println("===========================================")
+	log.Println("cd /")
 	if err := os.Chdir("/"); err != nil {
 		return fmt.Errorf("change dir to /: %w", err)
 	}
 
-	// TIPS: by unmounting, parent resource will be hidden from child process
 	if err := syscall.Unmount("/putold", syscall.MNT_DETACH); err != nil {
-		return fmt.Errorf("unmount pivot_root dir %w", err)
+		return fmt.Errorf("unmount old root dir %w", err)
 	}
 
-	// mouting /proc inside container
 	log.Println("mount /proc")
-	// NOTE: somehow mount /proc fails in lima & nerdctl with EPERM
-	if err := syscall.Mount("proc", "proc", "proc", 0, ""); err != nil {
+	if err := syscall.Mount("proc", "proc", "proc", syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV, ""); err != nil {
 		return fmt.Errorf("mounting new /proc in container: %w", err)
 	}
 
